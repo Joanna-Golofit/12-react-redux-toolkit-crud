3cb7facc68961cc87d94ddb918789ac8b3fd3fde
Joanna-Golofit <sunderlajsa@gmail.com>
2022-01-17 16:00:19 +0100

    deleteUser, updateUsername

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,57 @@
-import { useSelector } from 'react-redux';
-import './App.css';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { addUser, deleteUser, updateUsername } from "./reducers/Users";
 
 function App() {
-
-  const userList = useSelector((state) => state.users.value)
-
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newUsername, setNewUsername] = useState("");
   return (
     <div className="App">
       <div className="addUser">
-        <input type="text" placeholder="Name..." />
-        <input type="text" placeholder="Username..." />
-        <button>Add user</button>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            )
+          }
+        >
+          Add user
+        </button>
       </div>
-      <div className="displayUser">
+      <div className="displayUsers">
         {userList.map((user) => (
-          <>
-            <h1>{user.name}</h1>
-            <h2>{user.username}</h2>
-            <input type="text" className="text" />
-            <button>Change name</button>
-            <button>Change username</button>
-          </>
+          <div>
+            <p>{user.name}</p>
+            <p>{user.username}</p>
+            <input
+              type="text"
+              placeholder="New Username..."
+              className="text"
+              onChange={(e) => setNewUsername(e.target.value)}
+            />
+            <button onClick={() => dispatch(updateUsername({ id: user.id, username: newUsername }))}>Change Username</button>
+            <button onClick={() => dispatch(deleteUser({ id: user.id }))}>
+              Delete
+            </button>
+          </div>
         ))}
       </div>
     </div>
--- src/reducers/Users.js
+++ src/reducers/Users.js
@@ -7,9 +7,20 @@ export const userSlice = createSlice({
     value: usersData,
   },
   reducers: {
-    addUser: (state, action) => {},
+    addUser: (state, action) => {
+      state.value.push(action.payload);
+    },
+    deleteUser: (state, action) => {
+      state.value = state.value.filter((user) => user.id !== action.payload.id);
+    },
+    updateUsername: (state, action) => {
+      state.value.map((user) => {
+        if (user.id === action.payload.id) {
+        user.username = action.payload.username;
+      }});
+    },
   },
 });
 
-// export const { login, logout } = userSlice.actions;
+export const { addUser, deleteUser, updateUsername } = userSlice.actions;
 export default userSlice.reducer;
